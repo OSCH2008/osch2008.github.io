7a1744cd57abdcc512c370fbd692bad93ed4895a
OSCH2008 <82794982+OSCH2008@users.noreply.github.com>
2024-03-02 20:15:31 -0900

    js sucks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 var guh = document.getElementById("guh");
+var content = document.getElementById("content");
 
 function addGuh() {
     guh.innerHTML += " guh";
+    content.style.display = "block";
 }
